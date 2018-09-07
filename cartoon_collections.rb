32b8496d003c9e253ae9c5a38c4d200a50284809
N-S-practice-code <nico.ehs05@gmail.com>
2018-09-07 15:04:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.each_with_index do |el, i|
+  array.any? { |el| el.size>4 }
 end
 
 def find_the_cheese# code an argument here
