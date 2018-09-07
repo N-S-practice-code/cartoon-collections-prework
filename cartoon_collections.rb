cab9906d654b3ab9bc274774eae4494711c737f9
N-S-practice-code <nico.ehs05@gmail.com>
2018-09-07 14:59:17 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,15 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |el, i|
+    puts "#{}. #{}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map {|el| el}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.each_with_index do |el, i|
 end
 
 def find_the_cheese# code an argument here
